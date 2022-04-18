3bfe8e79baf01f296d995efe70ba65ac769ee8c3
Blake <blake.rxxk@gmail.com>
2022-04-18 20:58:06 +0300

    project init commit

--- BitPanda/AppDelegate.swift
+++ BitPanda/AppDelegate.swift
@@ -2,35 +2,24 @@
 //  AppDelegate.swift
 //  BitPanda
 //
-//  Created by Олег Кургаев on 12.04.2022.
+//  Created by Oleg Kurgaev on 12.04.2022.
 //
 
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
 
+  func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
+    window = UIWindow()
+    guard let window = window else { return false }
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
-    }
+    window.rootViewController = ViewController()
+    window.makeKeyAndVisible()
 
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
+    return true
+  }
 
 }
-
--- BitPanda/ViewController.swift
+++ BitPanda/ViewController.swift
@@ -2,18 +2,15 @@
 //  ViewController.swift
 //  BitPanda
 //
-//  Created by Олег Кургаев on 12.04.2022.
+//  Created by Oleg Kurgaev on 12.04.2022.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-

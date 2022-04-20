bcd109400d091299b230e144e35196e0709e8eca
Blake <blake.rxxk@gmail.com>
2022-04-20 16:31:56 +0300

    add spacing and themes

---
+++ Modules/BitPandaUI/Sources/BitPandaUI/Spacing.swift
@@ -0,0 +1,33 @@
+//
+//  Spacing.swift
+//  BitPandaUI
+//
+//  Created by Oleg Kurgaev on 20.04.2022.
+//
+
+import UIKit
+
+public enum Spacing {
+    public static let x4: CGFloat = 4
+    public static let x8: CGFloat = 8
+    public static let x12: CGFloat = 12
+    public static let x16: CGFloat = 16
+    public static let x24: CGFloat = 24
+    public static let x32: CGFloat = 32
+    public static let x40: CGFloat = 40
+    public static let x48: CGFloat = 48
+    public static let x56: CGFloat = 56
+    public static let x64: CGFloat = 64
+    public static let x72: CGFloat = 72
+    public static let x80: CGFloat = 80
+}
+
+public enum CornerRadius {
+    public static let x2: CGFloat = 2
+    public static let x4: CGFloat = 4
+    public static let x8: CGFloat = 8
+    public static let x12: CGFloat = 12
+    public static let x16: CGFloat = 16
+    public static let x24: CGFloat = 24
+    public static let round: CGFloat = 128
+}
